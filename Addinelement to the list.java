041fe250ce1395ed12e24655a73bc2446235f196
Kunal Vikas Kale <kkale8991@gmail.com>
2026-06-10 11:04:05 +0530

    Add files via upload

---
+++ Addinelement to the list.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Geeks{
+ public static void main( String [] args)
+   {
+     Collection <String> c = new ArrayList<>();
+     
+     
+     c.add("apple");
+     c.add("banana");
+     c.add("mango");
+     
+     
+     System.out.println("Collection" +c);
+     
+     }
+     }
