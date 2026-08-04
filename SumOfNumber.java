d276e0e6682bee38119527a86ebf32a7f39bac41
Kunal Vikas Kale <kkale8991@gmail.com>
2026-08-04 08:34:52 +0530

    Add files via upload

---
+++ SumOfNumber.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SumOfNumber{
+
+    public static void main (String args[])
+
+    {
+        int sum = 0, num;
+
+        System.out.println("Enter a number :");
+        Scanner scanner = new Scanner(System.in);
+        num = scanner.nextInt();
+
+        while(num >  0)
+
+           for (int i =0; sum = +1; sum ++)
+           {
+            System.out.println(sum);
+           }
+    }
+}
