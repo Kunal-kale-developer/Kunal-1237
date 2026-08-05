e6ad49e12369e05d3b75b41974022834ff064c90
Kunal Vikas Kale <kkale8991@gmail.com>
2026-08-05 19:20:22 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,19 @@
+public class Pattern
+{
+ public static void main(String [] args)
+  {
+  
+  int n = 4;
+  
+  for (int j =0; j <=n ; j++)
+   {
+    for (int i =0 ; i <=n; i++)
+	
+	{
+	
+	System.out.println("x" + "x");
+	}
+	
+	}
+	}
+	}
