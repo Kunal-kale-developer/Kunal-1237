3338cef88b4e8cac235ecf6454c9a91b4b972601
Kunal Vikas Kale <kkale8991@gmail.com>
2026-08-06 10:15:39 +0530

    Add files via upload

---
+++ Pattern2.java
@@ -0,0 +1,17 @@
+   public class Pattern2 
+    {
+	 public static void main (String args[])
+	 {
+	 
+	 for (int j = 0 ; j < 10 ; j++)
+	 
+	 {
+	   for (int i =0 ;  i < 100; i++)
+	   {
+	     System.out.println("X" + "k");
+		 
+		 }
+		 
+		 }
+		 }
+	}
