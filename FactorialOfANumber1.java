3b0c1ad5328809b2088499a6480c34b3d8cbf842
Kunal Vikas Kale <kkale8991@gmail.com>
2026-08-31 19:55:01 +0530

    Add files via upload

---
+++ FactorialOfANumber1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+ public class FactorialOfANumber1 {
+  public static void main (String args [])
+   {
+     int fact = 1;
+     
+     System.out.println("Enter a number :")
+     Scanner ip = new Scanner(System.in);
+     
+     int ip = ip.nextInt();
+     
+     while (num > 0)
+      {
+       fact = fact * num ;
+       num --;
+       }
+       
+       System.out.println("Factorail of a number is : " +fact);
+       }
+       ]
+       }
