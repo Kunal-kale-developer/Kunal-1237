2df245c66482aa8062fd983a1856426250f14ef1
Kunal Vikas Kale <kkale8991@gmail.com>
2026-09-07 10:54:31 +0530

    Add files via upload

---
+++ PronicNumber.java
@@ -0,0 +1,17 @@
+public class PronicNumber{
+  public static void main (String args [])
+   {
+    int num = 30;
+    
+    for (int i =1 ; i < num ; i ++)
+     {
+      int pronic = i * (i+1);
+       
+       
+       System.out.println("The pronic numbers are from 1 to 30 are " +pronic);
+       
+       
+       }
+       }
+       }
+
