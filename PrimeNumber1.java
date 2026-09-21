52e61d86f65b73c82e2efddbb0585b9ef45c9aca
Kunal Vikas Kale <kkale8991@gmail.com>
2026-09-21 12:22:08 +0530

    Add files via upload

---
+++ PrimeNumber1.java
@@ -0,0 +1,26 @@
+public class PrimeNumber
+ {
+  public static void main (String args []0
+   {
+    int num 1;
+    int den =7;
+    
+    while (num > den)
+    
+    {
+      if(num % 2 == 0) 
+      {
+       break ;
+       }
+       elseif(num == den)
+        {
+        System.out.println("The numbers is a prime number");
+        
+        }
+         }
+         }
+         else
+         {
+         System.out.println("The number is not a prime number ");
+         
+         }
